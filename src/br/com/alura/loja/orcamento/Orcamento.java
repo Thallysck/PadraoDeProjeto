e38ba95ef3c1a855af6e19e48123952e869a94eb
Thallys Rocha <thallys.brother@gmail.com>
2024-01-16 14:30:22 -0300

    utilizando a técnica State para desconto baseado nos estados do orcamento. Tecnica utilizada para a melhor prática ao inves de utilizar IF e Else

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,17 +1,22 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+    private SituacaoOrcamento situacao;
 
 
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
     }
 
     public BigDecimal getValor() {
@@ -19,5 +24,30 @@ public class Orcamento {
     }
     public int getQuantidadeItens() {
         return quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra(){
+        BigDecimal valorDescontoExtra = this.situacao.calcularDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
+
+    public void aprovar(){
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar(){
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar(){
+        this.situacao.finalizar(this);
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
     }
 }
